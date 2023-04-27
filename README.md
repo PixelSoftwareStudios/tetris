0c7011cf6f0c2ca093599230aa7ab394717e6d46
PixelSoftwareStudios <thegodpixel@protonmail.com>
2023-04-27 04:17:50 -0400

    Massive refactor, cleanup, fix movement

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # tetris
-Tetris in python
+## Tetris in Python
 
-This project was started on April 21st, 2023, after I watched the Tetris movie. What a good movie. Absolutely inspiring.
+This project was started on April 21st, 2023, after I watched the Tetris movie. What a good movie. Absolutely inspiring.  
 In that movie, they show the original version of Tetris made by Alexey Pajitnov.
-This is inspired by that version.
+This is inspired by that version.  
+I also watched [this video](https://www.youtube.com/watch?v=hVSGssuUbXo) to see the gameplay.
 
 Right now, it is very basic and only has simple block movement.
 It also prints each frame to stdout.
-I will eventually use curses to make it smoother.
+I will eventually use curses to make it like a real game and allow arrow key movement.
+Maybe even use the playsound module to play the tetris theme
--- tetris.py
+++ tetris.py
@@ -4,6 +4,7 @@
 # / ____/ />  </  __/ /___/ / /_/ / __/ /_ | |/ |/ / /_/ / /  /  __/__/ / /_/ /_/ / /_/ / / /_/ (__  )
 #/_/   /_/_/|_|\___/_//____/\____/_/  \__/ |__/|__/\__,_/_/   \___/____/\__/\__,_/\__,_/_/\____/____/
 #
+# Started: 04/21/2023
 
 # base = "<| . . . . . . . . . . |>"
 # base = "<| . . . . . . . . . . |>"
@@ -35,70 +36,72 @@ textris = " ___________________  ________\n/_  __/ __/_  __/ _ \/  _/ __/\n / / 
 base = "<| . . . . . . . . . .|>"
 floor = "<|====================|>\n \/\/\/\/\/\/\/\/\/\/\/  "
 
-# 0 represents empty
-# 1 represents a moving block
-# -1 represents a stationary block
-
 clearGameRow = [0,0,0,0,0,0,0,0,0,0]
 
 gameState = [list(clearGameRow) for _ in range(20)]
 
-# blockState = list(gameState)
+# Track the position of the current block's rows
+blockPosRows = []
 
-blockPosRows = [0,1]
+needToSpawn = True
 
-LShape = [
+gameScore = 0
+
+# 0 represents empty
+# 1 represents a moving block
+# -1 represents a stationary block
+
+# Possible block shapes
+IBlock = [
+	[0,0,0,1,1,1,1,0,0,0]
+]
+
+LBlock = [
 	[0,0,0,0,1,1,1,0,0,0],
 	[0,0,0,0,1,0,0,0,0,0]
 ]
 
-LineShape = [
-	[0,0,0,1,1,1,1,0,0,0]
-]
-
-SquareShape = [
+# rip von
+OBlock = [
 	[0,0,0,0,1,1,0,0,0,0],
 	[0,0,0,0,1,1,0,0,0,0]
 ]
 
-TShape = [
+TBlock = [
 	[0,0,0,0,1,1,1,0,0,0],
 	[0,0,0,0,0,1,0,0,0,0]
 ]
 
-ZLeftShape = [
+ZBlock = [
 	[0,0,0,0,1,1,0,0,0,0],
 	[0,0,0,0,0,1,1,0,0,0]
 ]
 
-ZRightShape = [
+SBlock = [
 	[0,0,0,0,0,1,1,0,0,0],
 	[0,0,0,0,1,1,0,0,0,0]
 ]
 
-Shapes = [LShape, LineShape, SquareShape, TShape, ZLeftShape, ZRightShape]
+shapes = [IBlock, LBlock, OBlock, TBlock, ZBlock, SBlock]
 
-needToSpawn = True
-
-spawnedShape = None
-
-gameScore = 0
-
-def spawnShape():
+def spawnBlock():
 	global needToSpawn
-	global spawnedShape
 	global blockPosRows
 	# Get a random shape from the list
-	spawnedShape = random.choice(Shapes)
+	spawnedBlock = random.choice(shapes)
 
 	# Set the shape in the game state
-	for i in range(0, len(spawnedShape)):
-		for j in range(0, len(spawnedShape[i])):
-			gameState[i][j] = spawnedShape[i][j]
-	blockPosRows = [0,1]
+	for i in range(0, len(spawnedBlock)):
+		for j in range(0, len(spawnedBlock[i])):
+			gameState[i][j] = spawnedBlock[i][j]
+
+	# Set the rows to the size of the block
+	blockPosRows = list(range(0, len(spawnedBlock)))
 	needToSpawn = False
 
 def printGameState():
+	print("")
+
 	for i in gameState:
 		# If the row is empty, print the base
 		if len(set(i)) == 1:
@@ -113,173 +116,125 @@ def printGameState():
 			builder += "|>"
 			print(builder)
 	print(floor)
+	print("Score:", gameScore)
 
-def checkForFullRows():
-	global gameScore
-	# Check each row
-	for i in range(0, len(gameState)):
-		# If the row is full
-		if len(set(gameState[i])) == 1 and gameState[i][0] == -1:
-			# Remove the row
-			gameState.pop(i)
-			# Add a new empty row
-			gameState.insert(0, clearGameRow)
-			# Increase the score
-			gameScore += 1
-			# Check for more full rows
-			checkForFullRows()
+# def checkForFullRows():
+# 	global gameScore
+# 	# Check each row
+# 	for i in range(0, len(gameState)):
+# 		# If the row is full
+# 		if len(set(gameState[i])) == 1 and gameState[i][0] == -1:
+# 			# Remove the row
+# 			gameState.pop(i)
+# 			# Add a new empty row
+# 			gameState.insert(0, clearGameRow)
+# 			# Increase the score
+# 			gameScore += 1
+# 			# Check for more full rows
+# 			checkForFullRows()
 
 # def checkForGameOver():
 # 	if (-1 in gameState[0]):
 # 		print("Game Over!")
 
-# def checkForShapeCollision():
-# 	for i in range(0, len(gameState)):
-# 		for j in range(0, len(gameState[i])):
-# 			if gameState[i][j] == 1:
-# 				if i + 1 <= len(gameState) - 1:
-# 					if gameState[i+1][j] == -1:
-# 						return True
-# 				else:
-# 					return True
-# 	return False
+def moveBlock(direction):
+	global needToSpawn
 
-def moveShape(direction):
 	if direction == "down":
-		# Only check the part of the gameState where the shape is
-		# Move the shape from bottom up to avoid collision issues
+		# Only check the part of the gameState where the block is
+		# Move the block from bottom up to avoid collision issues
 		for i in range(len(blockPosRows) - 1, -1, -1):
-			blockRow = gameState[blockPosRows[i]]
+			rowPos = blockPosRows[i]
+			if rowPos + 1 > len(gameState) - 1:
+				return
+			
+			blockRow = gameState[rowPos]
 
 			for j in range(0, len(blockRow)):
-				# If the current cell is part of the shape
+				# If the current cell is part of the block
 				if blockRow[j] == 1:
 					# If the row below is part of the board
-					if blockPosRows[i] + 1 <= len(gameState) - 1:
+					if rowPos + 1 <= len(gameState) - 1:
 						# The row below
-						nextBlockRow = gameState[blockPosRows[i]+1]
+						nextBlockRow = gameState[rowPos + 1]
 						# If the cell below is not empty
 						if nextBlockRow[j] == -1:
 							return
-						nextBlockRow[j] = 1
 						blockRow[j] = 0
+						nextBlockRow[j] = 1
 			# After moving down the row, move the pointer down
 			blockPosRows[i] += 1
+		
+		# If the block has hit the bottom, spawn a new block
+		if blockPosRows[-1] == len(gameState) - 1:
+			needToSpawn = True
 
-		# Old way
-		# # Ranges are exclusive, so we range to -1 to get to the 0th
-		# for i in range(len(gameState) - 1, -1, -1):
-		# 	for j in range(0, len(gameState[i])):
-		# 		# If the current cell is part of the shape
-		# 		if gameState[i][j] == 1:
-		# 			# Move the cell down
-		# 			if i + 1 <= len(gameState) - 1:
-		# 				gameState[i+1][j] = 1
-		# 				gameState[i][j] = 0
-		# 			else:
-		# 				return
 	if direction == "left":
 		for i in blockPosRows:
-			# Check if the shape is at the left edge
-			if gameState[i][0] != 0:
+			# Check if the block is at the left edge or has hit the bottom
+			if gameState[i][0] != 0 or i == len(gameState) - 1:
+				print("ahh")
 				return
 			# Shift the row to the left
 			gameState[i] = gameState[i][1:] + [0]
-		# Old way
-		# for i in range(0, len(gameState)):
-		# 	for j in range(0, len(gameState[i])):
-		# 		# If the current cell is part of the shape
-		# 		if gameState[i][j] == 1:
-		# 			if j - 1 < 0 or i >= len(gameState) - 2:
-		# 				return
-		# 			gameState[i][j] = 0
-		# 			gameState[i][j-1] = 1
+
 	if direction == "right":
 		for i in blockPosRows:
-			if gameState[i][-1] != 0:
+			# Check if the block is at the right edge or has hit the bottom
+			if gameState[i][-1] != 0 or i == len(gameState) - 1:
 				return
 			# Shift the row to the right
 			gameState[i] = [0] + gameState[i][:-1]
 
-		# Old way
-		# for i in range(0, len(gameState)):
-		# 	for j in range(len(gameState[i]) - 1, -1, -1):
-		# 		# If the current cell is part of the shape
-		# 		if gameState[i][j] == 1:
-		# 			# Move the cell right
-		# 			if j + 1 > len(gameState[i]) - 1 or i >= len(gameState) - 2:
-		# 				return
-		# 			gameState[i][j+1] = 1
-		# 			gameState[i][j] = 0
 	if direction == "drop":
 		for i in range(len(blockPosRows) - 1, -1, -1):
-			blockRow = gameState[blockPosRows[i]]
+			rowPos = blockPosRows[i]
+			blockRow = gameState[rowPos]
 
 			for j in range(0, len(blockRow)):
-				# If the current cell is part of the shape
+				# If the current cell is part of the block
 				if blockRow[j] == 1:
+					endRow = rowPos - 2 + len(gameState)
 					# If the row below is part of the board
-					if blockPosRows[i] + 1 <= len(gameState) - 1:
+					if endRow <= len(gameState) - 1:
 						# The row below
-						nextBlockRow = gameState[blockPosRows[i]+1]
+						nextBlockRow = gameState[endRow]
 						# If the cell below is not empty
-						if nextBlockRow[j] == -1:
+						if nextBlockRow[j] != 0:
 							return
-						nextBlockRow[j] = 1
-						blockRow[j] = 0
-			# After moving down the row, move the pointer down
-			blockPosRows[i] += 1
-		# rows = []
-		# for i in range(0, len(gameState)):
-		# 	if gameState[i].count(1) > 0:
-		# 		rows.append(i)
-	
-		# for i in rows:
-		# 	gameState[i] = list(clearGameRow)
 
-		# for i in range(0, len(spawnedShape)):
-		# 	for j in range(0, len(spawnedShape[i])):
-		# 		gameState[i - 2 + len(gameState)][j] = spawnedShape[i][j]
-		
-		# Old way 
-		# for i in range(len(gameState) - 1, -1, -1):
-		# 	for j in range(0, len(gameState[i])):
-		# 		# If the current cell is part of the shape
-		# 		if gameState[i][j] == 1:
-		# 			# Move the cell down
-		# 			# if i + 1 <= len(gameState) - 1:
-		# 			gameState[len(gameState) - 1][j] = -1
-		# 			gameState[i][j] = 0
-		# 			# else:
-					# 	return
-		# for i in range(len(gameState) - 1, -1, -1):
-		# 	for j in range(0, len(gameState[i])):
-		# 		# If the current cell is part of the shape
-		# 		if gameState[i][j] == 1:
-		# 			if (i < len(gameState) - 2):
-		# 				gameState[len(gameState) - 1][j] = -1
-		# 				gameState[i][j] = 0
-		# 			else:
-		# 				return
+						nextBlockRow[j] = -1
+						blockRow[j] = 0
+
+		needToSpawn = True
+
+def promptForMove():
+	move = input("Your move: ").lower()
+	if move in ["left", "l"]:
+		moveBlock("left")
+	elif move in ["right", "r"]:
+		moveBlock("right")
+	elif move in ["drop", "d"]:
+		moveBlock("drop")
+	elif move != "":
+		print("Unrecognized move")
 
 def main():
 	try:
 		print(textris)
-		print("Controls: left (l), right (r), drop (d)\n")
+		print("Controls: left (l), right (r), drop (d)")
+		# Game loop
 		while True:
 			if needToSpawn:
-				spawnShape()
+				spawnBlock()
 
 			printGameState()
-			print("Score:", gameScore)
-			move = input("Your move: ").lower()
-			if move in ["left", "l"]:
-				moveShape("left")
-			elif move in ["right", "r"]:
-				moveShape("right")
-			elif move in ["drop", "d"]:
-				moveShape("drop")
-			moveShape("down")
+
+			promptForMove()
+
+			moveBlock("down")
 	except KeyboardInterrupt:
 		print("\nExiting...")
-main()
+
+if __name__ == "__main__":
+	main()
